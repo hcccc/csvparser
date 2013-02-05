11de3177a6d0a538093d8259637c2dccffe0505c
hc0519 <reversal623@gmail.com>
2013-02-06 00:59:31 +0900

    start doing perl

--- test.pl
+++ test.pl
@@ -1,5 +1,22 @@
 #! /usr/bin/perl
 
+use strict;
+use warnings;
+
+$file = $ARGV[0];
+open(INFO, $file) or die("Can not read the input");
+
+$count = 0;
+
+foreach $line (<INFO>) {
+	chomp($line); #remove the new line
+	# <th colspan>  area name
+	# nowwrap 
+	#rd 
+	#<a href="scd007"
+
+}
+
 if ($ARGV[0] eq "あ"){
     print "Yes\n";
 }
