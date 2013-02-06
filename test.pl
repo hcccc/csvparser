f2b4e2c3b03bbf0372c8e71f2119293a1df7d641
hc0519 <reversal623@gmail.com>
2013-02-07 01:46:51 +0900

    0207

--- test.pl
+++ test.pl
@@ -3,18 +3,43 @@
 use strict;
 use warnings;
 
-$file = $ARGV[0];
+my $file = $ARGV[0];
 open(INFO, $file) or die("Can not read the input");
 
-$count = 0;
+my $count = 0;
+my @infolist;
 
-foreach $line (<INFO>) {
+my $bbtitle = "BBCP";
+my $p4utitle = "P4U";
+my $ggtitle = "GGAC";
+my $event = 0; 
+
+
+foreach my $line (<INFO>) {
 	chomp($line); #remove the new line
 	# <th colspan>  area name
 	# nowwrap 
 	#rd 
 	#<a href="scd007"
 
+	if ($line =~ $bbtitle) {
+		print "$line\n";
+	}
+
+	if ($line =~ $p4utitle) {
+		print "$line\n";
+	}
+
+	if ($line =~ $ggtitle) {
+		print "$line\n";
+	}
+
+	if ($line =~ m/エ.*リ.*ア/){
+		print "$line\n";
+	}
+
+	$infolist[$count] = $line;
+	$count++;
 }
 
 if ($ARGV[0] eq "あ"){
